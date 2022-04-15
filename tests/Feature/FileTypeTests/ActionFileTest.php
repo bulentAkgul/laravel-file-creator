9ed03a643a8c1879a767ac67f5cd2e72135b9393
Bulent Akgul <bulentakgul1980@gmail.com>
2022-04-15 20:42:31 +0300

    a minor change

--- tests/Feature/FileTypeTests/ActionFileTest.php
+++ tests/Feature/FileTypeTests/ActionFileTest.php
@@ -11,7 +11,7 @@ class ActionFileTest extends FileTestService
 
     public function __construct()
     {
-        parent::__construct('sl', true);
+        parent::__construct();
     }
 
     /** @test */
