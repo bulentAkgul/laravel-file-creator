7857d2588467ee4552c6092631ec94d3f10afcb6
Bulent Akgul <bulent.akgul@me.com>
2022-05-23 15:58:50 +0300

    dot notation view path generator added

---
+++ src/Functions/SetViewPath.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bakgul\FileCreator\Functions;
+
+use Bakgul\Kernel\Helpers\Settings;
+use Bakgul\Kernel\Helpers\Text;
+use Bakgul\Kernel\Tasks\ConvertCase;
+
+class SetViewPath
+{
+    public static function _($request)
+    {
+        $registrar = match (true) {
+            Settings::standalone('laravel') => '',
+            Settings::standalone('package') => Settings::identity('registrar'),
+            default => $request['attr']['package']
+        };
+
+        return Text::prepend($registrar, '::')
+            . str_replace(Settings::seperators('folder'), '.', implode('.', array_filter([
+                Settings::folders('apps'),
+                $request['attr']['app_folder'],
+                Settings::folders('view'),
+                ...array_filter(explode(':', $request['attr']['views'])),
+            ])))
+            . '.';
+    }
+}
