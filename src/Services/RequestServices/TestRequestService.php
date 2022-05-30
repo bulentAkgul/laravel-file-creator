e58b863e5155bbf64989c149811cf7072a3eaef8
Bulent Akgul <bulent.akgul@me.com>
2022-05-30 19:09:52 +0300

    isolated tests can be created as pair

--- src/Services/RequestServices/TestRequestService.php
+++ src/Services/RequestServices/TestRequestService.php
@@ -5,6 +5,9 @@ namespace Bakgul\FileCreator\Services\RequestServices;
 use Bakgul\Kernel\Helpers\Settings;
 use Bakgul\Kernel\Helpers\Folder;
 use Bakgul\FileCreator\Services\RequestService;
+use Bakgul\Kernel\Helpers\Arry;
+use Bakgul\Kernel\Helpers\Convention;
+use Bakgul\Kernel\Helpers\Text;
 
 class TestRequestService extends RequestService
 {
@@ -28,8 +31,28 @@ class TestRequestService extends RequestService
         return array_merge($map, [
             'task' => $attr['variation'] == 'feature' ? $map['task'] : '',
             'variation' => ucfirst($attr['variation']),
-            'folder' => $attr['variation'] == 'feature' &&  $attr['task'] ? Folder::name($attr['name'], $attr['type']) : '',
+            'folder' => $this->setFolder($attr)
         ]);
+    }
+
+    private function setFolder($attr)
+    {
+        return match (true) {
+            $this->hasTask($attr) => Folder::name($attr['name'], $attr['type']),
+            $this->isIsolated($attr) => 'IsolatedTests',
+            default => ''
+        };
+    }
+
+    private function hasTask($attr): bool
+    {
+        return $attr['variation'] == 'feature' &&  $attr['task'];
+    }
+
+    private function isIsolated($attr): bool
+    {
+        return count(array_filter($attr['queue'], fn ($x) => $x['type'] == 'test' && $x['status'] != 'main')) == 1
+            && Arry::value($attr['queue'], 'main', 'status', '=', 'type') != 'controller';
     }
 
     private function setTasks($attr)
@@ -45,6 +68,7 @@ class TestRequestService extends RequestService
 
     private function extendRequest(array $request): array
     {
+        $request['map']['subs'] = $this->extendSubs($request);
         $request['attr']['path'] = $this->makeReplacements($request, 'path');
 
         $request['map']['class'] = $this->makeReplacements($request, 'name_schema');
@@ -53,5 +77,19 @@ class TestRequestService extends RequestService
         $request['attr']['file'] = "{$request['map']['class']}.php";
 
         return $request;
+    }
+
+    private function extendSubs($request)
+    {
+        return Text::prepend(
+            $this->isIsolated($request['attr'])
+                ? "{$this->getMainType($request)}Tests"
+                : ''
+        ) . $request['map']['subs'];
+    }
+
+    private function getMainType($request)
+    {
+        return Convention::class(Arry::value($request['attr']['queue'], 'main', 'status', '=', 'type'));
     }
 }
