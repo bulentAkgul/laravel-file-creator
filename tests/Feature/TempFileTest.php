0b292ef90d7d6965b2f626c369c58ff424733031
Bulent Akgul <bulent.akgul@me.com>
2022-05-30 19:10:02 +0300

    minor change

--- tests/Feature/TempFileTest.php
+++ tests/Feature/TempFileTest.php
@@ -2,6 +2,7 @@
 
 namespace Bakgul\FileCreator\Tests\Feature;
 
+use Bakgul\Kernel\Helpers\Settings;
 use Bakgul\Kernel\Tests\Tasks\SetupTest;
 use Bakgul\Kernel\Tests\TestCase;
 
@@ -10,7 +11,8 @@ class TempFileTest extends TestCase
     /** @test */
     public function event_listener_provider()
     {
+        Settings::set('evaluator.evaluate_commands', false);
         $this->testPackage = (new SetupTest)();
-        $this->artisan('create:file dog listener testing -p=animal');
+        $this->artisan('create:file create-file-list action testing');
     }
 }
